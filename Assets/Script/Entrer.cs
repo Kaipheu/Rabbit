e3fb8fb0493f224aa0b1d2e53a921c138b388a2a
Kaipheu <djambaesaid73@gmail.com>
2018-07-14 17:54:36 +0200

    Modifications qui seront validées :  	nouveau fichier : Assets/Objet/CS.prefab  	nouveau fichier : Assets/Objet/CS.prefab.meta  	nouveau fichier : Assets/Objet/Couloir O.prefab  	nouveau fichier : Assets/Objet/Couloir O.prefab.meta  	nouveau fichier : Assets/Objet/SalleOT.prefab  	nouveau fichier : Assets/Objet/SalleOT.prefab.meta  	modifié :         Assets/Scene/s.unity  	nouveau fichier : Assets/Script/Entrer.cs  	nouveau fichier : Assets/Script/Entrer.cs.meta  	nouveau fichier : Assets/Script/Trigger.prefab  	nouveau fichier : Assets/Script/Trigger.prefab.meta  	nouveau fichier : Croqui/Sans titre.xcf  	nouveau fichier : Croqui/enigme.png  	modifié :         Library/AnnotationManager  	nouveau fichier : Library/CrashedAssetImports.txt  	nouveau fichier : Library/EditorInstance.json  	modifié :         Library/InspectorExpandedItems.asset  	modifié :         Library/ScriptAssemblies/Assembly-CSharp.dll  	modifié :         Library/ScriptAssemblies/Assembly-CSharp.dll.mdb  	modifié :         Library/assetDatabase3  	nouveau fichier : Library/metadata/0a/0a7ce40edbc5e42e2b90da8b892cf3b5  	nouveau fichier : Library/metadata/0a/0a7ce40edbc5e42e2b90da8b892cf3b5.info  	nouveau fichier : Library/metadata/17/1708726318d1c4a27b8071ef30e5a71b  	nouveau fichier : Library/metadata/17/1708726318d1c4a27b8071ef30e5a71b.info  	nouveau fichier : Library/metadata/3e/3e86d53564c80484480a7c1554d9666c  	nouveau fichier : Library/metadata/3e/3e86d53564c80484480a7c1554d9666c.info  	nouveau fichier : Library/metadata/ac/accb4cd435ef8414a9d03693a55965dc  	nouveau fichier : Library/metadata/ac/accb4cd435ef8414a9d03693a55965dc.info  	nouveau fichier : Library/metadata/bf/bfec682b25ddf4234b4f31581a4928b6  	nouveau fichier : Library/metadata/bf/bfec682b25ddf4234b4f31581a4928b6.info  	modifié :         Library/shadercompiler-UnityShaderCompiler0.log  	modifié :         ProjectSettings/ProjectSettings.asset  	modifié :         Rabbit.userprefs  	nouveau fichier : Temp/UnityLockfile  	nouveau fichier : Temp/UnityTempFile-236a1d52873c240d387312315863785d  	nouveau fichier : Temp/UnityTempFile-5c3cc9a68123d407e912a15a45e6a2f4  	nouveau fichier : Temp/UnityTempFile-6c7be46de2ab44958a45186a2c07e53f  	nouveau fichier : Temp/UnityTempFile-717435347d7684614a0dafdf21d9cf58  	nouveau fichier : Temp/UnityTempFile-73f4505d6337347df8d202ba511db237  	nouveau fichier : Temp/UnityTempFile-94fac8b6a08f54122a8711d708d2c2b3  	nouveau fichier : Temp/UnityTempFile-cdf5365e47a1047409851b6101b4ead2  	nouveau fichier : Temp/UnityTempFile-e82e0d9311c144e99a190bef89255649  	nouveau fichier : Temp/__Backupscenes/0.backup

---
+++ Assets/Script/Entrer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entrer : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("Entrer !");
+	}
+}
